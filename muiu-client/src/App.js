971d47626d4938f7434c074658dd807d6f840470
soojeong <soojeongmin@soongsil.ac.kr>
2024-09-23 15:52:49 +0900

    0923

--- muiu-client/src/App.js
+++ muiu-client/src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout';
+import Main from './pages/Main';
+import Login from './pages/Login';
+import Join from './pages/Join';
+import MyPage from './pages/MyPage';
+import MindCheck from './pages/MindCheck';
+import MyDiary from './pages/MyDiary';
+import MindColumn from './pages/MindColumn';
+import DisasterMentalHealthManual from './pages/DisasterMentalHealthManual';
+import DisasterGuide from './pages/DisasterGuide';
+import DisasterSafetyStore from './pages/DisasterSafetyStore';
+import HospitalShelterInfo from './pages/HospitalShelterInfo';
+import HumanCounseling from './pages/HumanCounseling';
+import AICounseling from './pages/AICounseling';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="main" element={<Main />} />
+          <Route path="login" element={<Login />} />
+          <Route path="join" element={<Join />} />
+          <Route path="mypage" element={<MyPage />} />
+          <Route path="mind-check" element={<MindCheck />} />
+          <Route path="human-counseling" element={<HumanCounseling />} />
+          <Route path="ai-counseling" element={<AICounseling />} />
+          <Route path="my-diary" element={<MyDiary />} />
+          <Route path="mind-column" element={<MindColumn />} />
+          <Route path="disaster-mental-health-manual" element={<DisasterMentalHealthManual />} />
+          <Route path="disaster-guide" element={<DisasterGuide />} />
+          <Route path="disaster-safety-store" element={<DisasterSafetyStore />} />
+          <Route path="hospital-shelter-info" element={<HospitalShelterInfo />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
