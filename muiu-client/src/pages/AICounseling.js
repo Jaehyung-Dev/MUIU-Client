10cd5044e31aae66377707caa16e79aba5d89228
ChungDaeun <daeunchungb2m@gmail.com>
2024-09-24 17:53:38 +0900

    240924 정다은 ai상담 메인페이지 제작중

--- muiu-client/src/pages/AICounseling.js
+++ muiu-client/src/pages/AICounseling.js
@@ -1,8 +1,51 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const ConsultationSection = styled.section`
+    text-align: center;
+    padding: 0;
+    flex: 1;
+`;
+
+const ImageBanner = styled.div`
+    width: 100%;
+    margin: 0;
+
+    img {
+        width: 100%;
+        height: auto;
+        object-fit: cover;
+    }
+`;
+
+const Title = styled.h1`
+    margin-top: 5vh;
+    margin-bottom: 8vh;
+
+    @media (max-width: 393px) {
+        font-size: 20px;
+    }
+`;
+
+const ShortHr = styled.hr`
+    width: 200px;
+    border: 0.5px solid #878787;
+    margin: 10px auto 7vh auto;
+
+    @media (max-width: 393px) {
+        width: 150px;
+    }
+`;
 
 export const AICounseling = () => {
     return (
-        <div>AICounseling</div>
+        <ConsultationSection>
+            <ImageBanner>
+                <img src={`${process.env.PUBLIC_URL}/images/AI_header_img.png`} alt='긴급상담 헤더 이미지' />
+            </ImageBanner>
+            <Title>긴급상담</Title>
+            <ShortHr />
+        </ConsultationSection>
     );
 };
 
