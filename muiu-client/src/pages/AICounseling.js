e44bad1a2d93c9d840747ab64e6fc375f98993e2
ssongmingyo <alsrysong@gmail.com>
2024-09-24 14:05:18 +0900

    first-commit

---
+++ muiu-client/src/pages/AICounseling.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const AICounseling = () => {
+    return (
+        <div>AICounseling</div>
+    );
+};
+
+export default AICounseling;
---
+++ muiu-client/src/pages/DisasterMentalHealthManual.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const DisasterMentalHealthManual = () => {
+    return (
+        <div>DisasterMentalHealthManual</div>
+    );
+};
+
+export default DisasterMentalHealthManual;
