bb62e51aecffac4e25a53f88439fceb9565e610f
ChungDaeun <daeunchungb2m@gmail.com>
2024-09-30 16:28:49 +0900

    Modify Main.js 작업 진행중

--- muiu-client/src/pages/Main.js
+++ muiu-client/src/pages/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { MdGpsFixed } from "react-icons/md";
 
 const Content = styled.div`
     margin-top: 62px;
@@ -9,6 +10,12 @@ const Content = styled.div`
 const Section = styled.div`
 
     padding: 20px;
+
+    h2 {
+        font-size: 1.2rem;
+        display: flex;
+        align-items: center;
+    }
 
     .bookmark-group {
         display: flex;
@@ -57,6 +64,54 @@ const Section = styled.div`
             }
         }
     }
+
+    .current-info-items {
+        width: 100%;
+        height: 90px;
+        display: flex;
+        flex-direction: column;
+        overflow-y: auto;
+        justify-content: space-around;
+
+        .current-info-item {
+            width: 100%;
+            height: 40px;
+            display: flex;
+
+            .current-location {
+                width: 12%;
+                height: 40px;
+                font-weight: bold;
+                font-size: 13px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                margin-right: 0.5rem;
+
+                p{
+                    text-align: center;
+                    text-wrap: wrap;
+                    font-size: 0.8rem;
+                    font-weight: bold;
+                }
+            }
+
+            .current-location-info {
+                width: 88%;
+                height: 40px;
+                font-size: 13px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+
+                p{
+                    text-align: left;
+                    text-wrap: wrap;
+                    font-size: 0.8rem;
+                }
+            }
+        }
+    }
 `;
 
 const Banner = styled.div`
@@ -71,47 +126,106 @@ const Banner = styled.div`
     margin: 0 auto;
 `;
 
+const Blocks = styled.div`
+    padding: 20px;
+    background-color: #F4F4F4;
+    display: flex;
+`;
+
+const Block = styled.div`
+    border-radius: 10px;
+    background-color: white;
+    // margin: 5px
+    padding: 10px;
+    
+    .block-text-bold {
+        font-weight: bold;
+    }
+
+    .block-text-small {
+        font-weight: normal;
+        font-size: 10px;
+        color: gray;
+    }
+`;
+
 export const Main = () => {
     return (
-        <Content>
-            <Section>
-                <h2>상담 전 내 마음 알아보기</h2>
-                <div class="bookmark-group">
-                    <div class="bookmark">
-                        <div class="bookmark-img">
-                            <img src="/images/Main/bookmark.svg" alt="bookmark"/>
+        <>
+            <Content>
+                <Section>
+                    <h2>상담 전 내 마음 알아보기</h2>
+                    <div class="bookmark-group">
+                        <div class="bookmark">
+                            <div class="bookmark-img">
+                                <img src="/images/Main/bookmark.svg" alt="bookmark"/>
+                            </div>
+                            <div class="bookmark-title">상담접수지</div>
+                            <div class="bookmark-content">
+                                <p>내 상황<br/>전달하기</p>
+                            </div>
                         </div>
-                        <div class="bookmark-title">상담접수지</div>
-                        <div class="bookmark-content">
-                            <p>내 상황<br/>전달하기</p>
+
+                        <div class="bookmark">
+                            <div class="bookmark-img">
+                                <img src="/images/Main/bookmark.svg" alt="bookmark"/>
+                            </div>
+                            <div class="bookmark-title">상담접수지</div>
+                            <div class="bookmark-content">
+                                <p>내 상황<br/>전달하기</p>
+                            </div>
+                        </div>
+
+                        <div class="bookmark">
+                            <div class="bookmark-img">
+                                <img src="/images/Main/bookmark.svg" alt="bookmark"/>
+                            </div>
+                            <div class="bookmark-title">상담접수지</div>
+                            <div class="bookmark-content">
+                                <p>내 상황<br/>전달하기</p>
+                            </div>
+                        </div>  
+                    </div>
+                </Section>
+                <Banner/>
+                <Section>
+                    <h2>서울시 관악구&nbsp;&nbsp;
+                        <MdGpsFixed size="1.2rem"/>
+                    </h2>
+                    <div class="current-info-items">
+                        <div class="current-info-item">
+                            <div class="current-location">
+                                <p>봉천동</p>
+                            </div>
+                            <div class="current-location-info">
+                                <p>관악로 21-1 도로공사로 인해 교통체증이 예상됩니다. 교통사고에 유의하세요.</p>
+                            </div>
+                        </div>
+
+                        <div class="current-info-item">
+                            <div class="current-location">
+                                <p>봉천동</p>
+                            </div>
+                            <div class="current-location-info">
+                                <p>관악로 21-1 도로공사로 인해 교통체증이 예상됩니다. 교통사고에 유의하세요.</p>
+                            </div>
                         </div>
                     </div>
-
-                    <div class="bookmark">
-                        <div class="bookmark-img">
-                            <img src="/images/Main/bookmark.svg" alt="bookmark"/>
-                        </div>
-                        <div class="bookmark-title">상담접수지</div>
-                        <div class="bookmark-content">
-                            <p>내 상황<br/>전달하기</p>
-                        </div>
-                    </div>
-
-                    <div class="bookmark">
-                        <div class="bookmark-img">
-                            <img src="/images/Main/bookmark.svg" alt="bookmark"/>
-                        </div>
-                        <div class="bookmark-title">상담접수지</div>
-                        <div class="bookmark-content">
-                            <p>내 상황<br/>전달하기</p>
-                        </div>
-                    </div>  
-                </div>
-            </Section>
-            <Banner>
-                {/* <img src='/images/Main/test-img.svg' alt="광고"/> */}
-            </Banner>
-        </Content>
+                </Section>
+            </Content>
+            <Blocks>
+                <Block>
+                    <div className="block-text-bold">안녕하세요. 서준님,</div>
+                    <div className="block-text-small">오늘의 하루는<br/>어떠셨나요?</div>
+                    <img src="/images/Main/graphImg.svg" alt="graphImg"/>
+                </Block>
+                <Block>
+                    <div className="block-text-small" style={{textAlign: 'right'}}>평생 써먹는<br/>자존감 높이는법</div>
+                    <div className="block-text-bold" style={{textAlign: 'right'}}>정다은 상담사</div>
+                    <img src="/images/Main/counsel-ex.svg" alt="counsel-ex"/>
+                </Block>
+            </Blocks>
+        </>
     );
 };
 
