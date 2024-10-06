9eaf5a0400a1220f5759f6f6e694767c11fd1c68
Chung-Daeun <daeunchungb2m@gmail.com>
2024-10-07 01:34:51 +0900

    확인차 업로드

--- src/pages/MyDiaryWrite.js
+++ src/pages/MyDiaryWrite.js
@@ -1,20 +1,14 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-import EditIcon from '@mui/icons-material/Edit';
-import ShareIcon from '@mui/icons-material/Share';
-import DeleteIcon from '@mui/icons-material/Delete';
 
 const Container = styled.div`
     margin-top: -10px;
     height: 100vh;
     width: 100vw;
     max-width: 600px;
-    background-color: #f3f3f3;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -60,8 +54,8 @@ const DiartTitle = styled.div`
     border: 0;
     font-size: 1.3rem;
     padding: 0 8px;
-    font-family: inherit; /* 추가 */
-    box-sizing: border-box; /* 추가 */
+    font-family: inherit; 
+    box-sizing: border-box; 
   }
 
   input:focus {
@@ -90,141 +84,62 @@ const DiartContent = styled.div`
   }
 `;
 
-const EntryHeader = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 10px;
-    position: relative;
-`;
-
-const TimeBlock = styled.div`
-    display: inline-flex;
-    align-items: center;
-    margin-bottom: 10px;
-    gap: 5px;
-    background-color: #34C759;
-    padding: 5px 10px;
-    border-radius: 5px;
-`;
-
-const EntryDateText = styled.span`
-    font-size: 14px;
-    color: #555;
-`;
-
-const EntryTitle = styled.h2`
-    padding-top: 10px;
-    padding-bottom: 10px;
-    margin: 10px 0;
-    font-size: 18px;
-    font-weight: bold;
-`;
-
-const EntryContent = styled.p`
-    margin: 10px 0;
-    font-size: 14px;
-    color: #333;
-    line-height: 1.5;
-`;
-
 const EmotionSection = styled.div`
     width: 80%; 
     padding: 20px;
     border-radius: 8px;
     background-color: white;
     box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.15);
-`;
-
-const CommentTitle = styled.h3`
-    margin: 0 0 10px 0;
-    font-size: 16px;
-`;
-
-const CommentWriter = styled.p`
-    margin: 0;
-    font-size: 14px;
-    font-weight: bold;
-    color: #555;
-`;
-
-const CommentContent = styled.p`
-    margin: 10px 0;
-    font-size: 14px;
-    color: #333;
-`;
-
-const CommentInputSection = styled.div`
-    width: 100%;
     display: flex;
-    background-color: #EEEEEE;
-    padding: 0;
-    margin-top: 25px;
-    margin-bottom: 10px;
-    box-sizing: border-box;
-    overflow: hidden;
+    justify-content: space-between;
 `;
 
-const CommentInput = styled.input`
-    flex: 1;
-    border: none;
-    outline: none;
-    background-color: #EEEEEE;
-    padding: 15px;
-    font-size: 14px;
-`;
-
-const SubmitButton = styled.button`
-    width: 80px;
-    background-color: #D9D9D9;
-    border: none;
-    font-size: 14px;
-    font-weight: bold;
-    cursor: pointer;
+const EmotionDiv = styled.div`
+    margin: 0.2rem;
+    width: 25%;
+    height: 15vh;
+    border-radius: 10px;
     display: flex;
-    align-items: center;
     justify-content: center;
-    color: black;
-
-    &:hover {
-        background-color: #a0a0a0;
-        color: black;
-    }
-`;
-
-const MenuContainer = styled.div`
-    position: absolute;
-    top: 40px;
-    right: 0;
-    width: 120px;
-    background-color: white;
-    border: 1px solid #e0e0e0;
-    border-radius: 8px;
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.15);
-    z-index: 1000;
-    padding: 10px;
-`;
-
-const MenuItem = styled.div`
-    display: flex;
     align-items: center;
-    gap: 10px;
-    padding: 8px;
-    cursor: pointer;
+    background-color: ${({ mood }) => {
+    switch(mood) {
+      case 'dissatisfied':
+        return '#FF3B30';
+      case 'bad':
+        return '#FF9500';
+      case 'soso':
+        return '#FFCC00';
+      case 'good':
+        return '#34C759';
+      case 'happy':
+        return '#00C7BE';
+      default:
+        return 'gray';
+        }
+    }};
+    @media screen and (max-width: 600px) {
+        width: 40%;
+        height: 10vh;
+    }
 
-    &:hover {
-        background-color: #f0f0f0;
+    img {
+        width: 100%;
+        height: 8vh;
+        margin-top: 0.5rem;
     }
 `;
 
-const SaveBtn = styled.button``;
+const SaveBtn = styled.button`
+    width: 80%; 
+    padding: 20px;
+    border-radius: 8px;
+    background-color: black;
+    color: white;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.15);
+`;
 
 const MyDiaryWrite = () => {
-  const [menuVisible, setMenuVisible] = useState(false);
-
-    const toggleMenu = () => {
-        setMenuVisible(!menuVisible);
-    };
 
   return (
     <Container>
@@ -248,7 +163,21 @@ const MyDiaryWrite = () => {
         </form>
       </DiaryEntry>
       <EmotionSection>
-
+        <EmotionDiv mood='dissatisfied'>
+          <img src='../svg/angry.svg'/>
+        </EmotionDiv>
+        <EmotionDiv mood='bad'>
+          <img src='../svg/depress.svg'/>
+        </EmotionDiv>
+        <EmotionDiv mood='soso'>
+          <img src='../svg/normal.svg'/>
+        </EmotionDiv>
+        <EmotionDiv mood='good'>
+          <img src='../svg/good.svg'/>
+        </EmotionDiv>
+        <EmotionDiv mood='happy'>
+          <img src='../svg/happy.svg'/>
+        </EmotionDiv>
       </EmotionSection>
       <SaveBtn></SaveBtn>
     </Container>
