2c30c45051d10e2308daeec663a63f0a6c357d76
ChungDaeun <daeunchungb2m@gmail.com>
2024-10-07 20:29:14 +0900

    Modify Main.js, MindCheck.js, MindColumn.js

--- src/pages/MindCheck.js
+++ src/pages/MindCheck.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React from 'react'
+import styled from 'styled-components'
 
-export const MindCheck = () => {
-    return (
-        <div>MindCheck</div>
-    );
-};
+const Banner = styled.div`
+    background-image: url('${process.env.PUBLIC_URL}/images/mind-check-header.png');
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    width: 100%;
+    height: 300px;
 
-export default MindCheck;
+    @media (max-width: 400px){
+        height: 200px;
+    }
+
+    @media (max-width: 500px){
+        height: 230px;
+    }
+`;
+
+const Board = styled.div`
+    background-image: url('${process.env.PUBLIC_URL}/images/mind-check-board.png');
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    width: 100%; 
+    height: 600px;
+
+    @media (max-width: 400px){
+        height: 400px;
+    }
+
+    @media (max-width: 500px){
+        height: 500px;
+    }
+`;
+
+const MindCheck = () => {
+  return (
+    <>
+        <Banner></Banner>
+        <Board></Board>
+    </>
+  )
+}
+
+export default MindCheck
