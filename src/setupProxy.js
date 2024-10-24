a5100ff66ced24cacf79563aecda1f3076f2061c
kimdaehwi990731 <whee990731@naver,com>
2024-10-24 12:27:57 +0900

    setupProxy.js 주석 추가

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
     /* 네이버 지도 api proxy 설정 */
     app.use(
-        '/api', // path parameter
+        '/api', // api path parameter
         createProxyMiddleware({
             target: 'https://naveropenapi.apigw.ntruss.com', //endpoint
             changeOrigin: true,
@@ -12,7 +12,7 @@ module.exports = function(app) {
 
     /* 네이버 로그인 api proxy 설정 */
     app.use(
-        '/api2', // api path parameter
+        '/api2', // api path parameter -- 재형님이 쓰시는 api url 사용하시면 돼요!
         createProxyMiddleware({
             target: '"https://nid.naver.com', // endpoint
             changeOrigin: true,
