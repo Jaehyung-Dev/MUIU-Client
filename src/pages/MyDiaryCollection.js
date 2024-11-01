3413a612035c32bb97fa783e250debe724897197
ChungDaeun <daeunchungb2m@gmail.com>
2024-11-01 16:00:14 +0900

    Modify MyDiaryCollection.js - 버튼 수정

--- src/pages/MyDiaryCollection.js
+++ src/pages/MyDiaryCollection.js
@@ -167,12 +167,20 @@ const ButtonBar = styled.div`
     width: 85%;
     box-sizing: border-box;
     margin-top: 1rem;
-    padding: 0 1rem;
     display: flex;
     justify-content: space-between;
 
     .click:hover {
         cursor: pointer;
+    }
+
+    div {
+        width: 45%;
+        background-color: white;
+        border-radius: 5px;
+        color: black;
+        text-align: center;
+        padding: 0.3rem;
     }
 `;
 
@@ -253,8 +261,10 @@ const MyDiaryCollection = () => {
                     <WeekCalendar />
                 </CalendarDiv>
                 <ButtonBar>
-                    <TimelineIcon onClick={handleChartClick} className='click'/>
-                    <CreateIcon onClick={handleWriteClick} className='click'/>
+                    {/* <TimelineIcon onClick={handleChartClick} className='click'/> */}
+                    {/* <CreateIcon onClick={handleWriteClick} className='click'/> */}
+                    <div onClick={handleChartClick} className='click'>분석보기</div>
+                    <div onClick={handleWriteClick} className='click'>일기쓰기</div>
                 </ButtonBar>
                 <MD_Block/>
             </CoverDiv>
